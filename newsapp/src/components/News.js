57e7b61c05dcde2c8a7a884811d13c268619d6e3
Chitranjan <chitranjangupta51@gmail.com>
2023-02-22 01:31:41 +0530

    Badge Added, time added, author name added

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -82,6 +82,9 @@ export class News extends Component {
                       }
                       imageUrl={element.urlToImage}
                       newsUrl={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}
                     ></NewsItem>
                   </div>
                 );
